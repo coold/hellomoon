3a121ef229c7171756a5518a1bd8ef130fe86e5b
coold <rdcoold@gmail.com>
2015-02-15 23:18:00 +0500

    audio

---
+++ app/src/main/java/ru/coold/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,68 @@
+package ru.coold.hellomoon;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.VideoView;
+
+/**
+ * Created by rz on 15.02.2015.
+ */
+public class HelloMoonFragment extends Fragment {
+    private AudioPlayer mPlayer = new AudioPlayer();
+    private Button mPlayButton;
+    private Button mStopButton;
+    private Button mPauseButton;
+    private VideoView mVideoView;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
+
+        mVideoView = (VideoView)v.findViewById(R.id.hellomoon_videView);
+        Uri resourceUri = Uri.parse("android.resource://ru.coold.hellomoon/"+R.raw.apollo_17_stroll);
+        try {
+            mVideoView.setVideoURI(resourceUri);
+        } catch (Exception e) {
+            Log.d("error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        mPauseButton = (Button)v.findViewById(R.id.hellomoon_pauseButton);
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.pause();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
+    }
+}
